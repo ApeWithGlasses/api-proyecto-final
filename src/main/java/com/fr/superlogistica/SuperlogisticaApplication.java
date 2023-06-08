f68debad88b83e25bf91270bd3b516e5606bd925
Firmware-R3bels <dalesjob7@gmail.com>
2023-06-08 14:18:52 -0500

    Initial commit

---
+++ src/main/java/com/fr/superlogistica/SuperlogisticaApplication.java
@@ -0,0 +1,13 @@
+package com.fr.superlogistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperlogisticaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperlogisticaApplication.class, args);
+	}
+
+}
